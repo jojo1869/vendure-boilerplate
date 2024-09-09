e0d0b877e8b8930b334ca4085f55bba20ec1d26b
jojo1869 <jorjohns87@gmail.com>
2024-09-09 02:21:53 -0700

    +

--- vendure-storefront/app/routes/products/$slug.tsx
+++ vendure-storefront/app/routes/products/$slug.tsx
@@ -129,7 +129,7 @@ export default function ProductSlug() {
               <ScrollableContainer>
                 {product.assets.map((asset) => (
                   <div
-                    className={/*`basis-1/3 md:basis-1/4*/`basis-1 md:basis-1 flex-shrink-0 select-none touch-pan-x rounded-lg ${
+                    className={`basis-1 md:basis-1/4 flex-shrink-0 select-none touch-pan-x rounded-lg ${
                       featuredAsset?.id == asset.id
                         ? 'outline outline-2 outline-primary outline-offset-[-2px]'
                         : ''
